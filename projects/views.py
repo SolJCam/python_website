d8e2cfc40891c2e02ca5be8e764712c48f390da7
SolJCam <scameron110@gmail.com>
2019-06-29 16:45:55 -0400

    added view behaviour, urls and html templates

--- projects/views.py
+++ projects/views.py
@@ -8,3 +8,10 @@ def project_index():
         'projects': projects
     }
     return render(request, 'project_index.html', context)
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
