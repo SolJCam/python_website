95c3c683eeeae39ff1e3dd82bf03bbcbbee357c2
SolJCam <scameron110@gmail.com>
2019-06-30 13:55:12 -0400

    base.html, views project_index  and models __str__ func modifications

--- projects/models.py
+++ projects/models.py
@@ -6,3 +6,5 @@ class Project(models.Model):
     description = models.TextField()
     technology = models.CharField(max_length=20)
     image = models.FilePathField(path="/img")
+    def __str__(self): #return a more helpful representation of this object
+        return self.title
--- projects/views.py
+++ projects/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from projects.models import Project
 
 # Create your views here.
-def project_index():
+def project_index(request):
     projects = Project.objects.all()
     context = {
         'projects': projects
