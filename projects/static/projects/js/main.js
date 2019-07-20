81b971d51c0eb089526b45fa7e73c3470f6a9c13
SolJCam <scameron110@gmail.com>
2019-07-20 16:58:31 -0400

    continued nav edits

--- projects/static/projects/js/main.js
+++ projects/static/projects/js/main.js
@@ -7,18 +7,42 @@ $(window).on("load", function (e) {
   console.log('2ndplz!');
 });
 
+
 //toggle between nav links and buttons based on viewport width
 window.matchMedia("(max-width:992px)").matches ? $(".pills").toggle() : window.matchMedia('(min-width:993px)').matches ? $(".btn-info").toggle() : null;
 
 
 //nav link animated display
 if (window.matchMedia("(min-width:992px)").matches) {
-  $(".pills").hover(  
-        function(){
-          $(".nav-item").toggleClass('d-lg-none');
-          $(".nav-img").toggleClass('d-lg-block');
-      }, function(){
-          $(".nav-item").toggleClass('d-lg-none');
-          $(".nav-img").toggleClass('d-lg-block');
-      });
+
+  $("#resume-nav").hover(
+    function(){
+      $("#S").toggleClass('d-lg-none');
+      $("#res-nav").toggleClass('d-lg-block');
+    },
+    function(){
+      $("#S").toggleClass('d-lg-none');
+      $("#res-nav").toggleClass('d-lg-block');
+    }
+  );
+  $("#project-nav").hover(
+    function(){
+      $("#P").toggleClass('d-lg-none');
+      $("#proj-nav").toggleClass('d-lg-block');
+    },
+    function(){
+      $("#P").toggleClass('d-lg-none');
+      $("#proj-nav").toggleClass('d-lg-block');
+    }
+  );
+  $("#connect-nav").hover(
+    function(){
+      $("#C",).toggleClass('d-lg-none');
+      $("#conn-nav").toggleClass('d-lg-block');
+    },
+    function(){
+      $("#C",).toggleClass('d-lg-none');
+      $("#conn-nav").toggleClass('d-lg-block');
+    }
+  );  
 }
