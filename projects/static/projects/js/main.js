83341168ddf9927d57af05f641ab829dbeb8cebd
SolJCam <scameron110@gmail.com>
2019-08-03 19:17:18 -0400

    further responsive touch ups; dated social icons

--- projects/static/projects/js/main.js
+++ projects/static/projects/js/main.js
@@ -1,12 +1,66 @@
 console.log("scripts are being read");
 
 //fade in my image and blurb
-$(window).on("load", function (e) {
+$(document).ready(function (e) {
   $("#me").fadeIn(2e3);
   $("#blurb").fadeIn(2e3);
     
   console.log('2ndplz!');
 });
+
+
+ 
+
+
+
+if (window.matchMedia("(min-width:992px)").matches) {
+
+  //toggle email position; DO NOT REMOVE. Bootstrap works from small to larg views. The below function is the only way to remove this class for larger viewports  
+  $(".email").toggleClass('mr-3 ml-3'); 
+
+  //nav links animated display
+  $("#resume-nav").hover(
+    function(){
+      $("#R").toggleClass('d-lg-none');
+      $("#res-nav").toggleClass('d-lg-block');
+    },
+    function(){
+      $("#R").toggleClass('d-lg-none');
+      $("#res-nav").toggleClass('d-lg-block');
+    }
+  );
+  $("#project-nav").hover(
+    function(){
+      $("#P").toggleClass('d-lg-none');
+      $("#proj-nav").toggleClass('d-lg-block');
+    },
+    function(){
+      $("#P").toggleClass('d-lg-none');
+      $("#proj-nav").toggleClass('d-lg-block');
+    }
+  );
+  $("#connect-nav").hover(
+    function(){
+      $("#C").toggleClass('d-lg-none');
+      $("#conn-nav").toggleClass('d-lg-block');
+    },
+    function(){
+      $("#C").toggleClass('d-lg-none');
+      $("#conn-nav").toggleClass('d-lg-block');
+    }
+  );
+
+// toggle nav link width. Doesn't have breakpoint support
+  $("#resume-nav").toggleClass("no-gutters");
+  $("#project-nav").toggleClass("no-gutters");
+  $("#connect-nav").toggleClass("no-gutters");
+
+}
+
+
+
+
+
 
 //Social Media Link Behaviour
 $("#first").hover(
@@ -41,56 +95,11 @@ $("#third").hover(
     $("#second").fadeTo("fast", 1);
   }
 )
- 
-if (window.matchMedia("(min-width:992px)").matches) {
 
-  //shift name and nav depending on screen size
-  $(".two").toggleClass("offset-sm-1");
-  $(".two").toggleClass("offset-sm-2");
+// Not sure if this code is necessary. Browser resize suggests yes, Chrome dev tools suggests no. Will know after deployment
 
-  //nav links animated display
-  $("#resume-nav").hover(
-    function(){
-      $("#R").toggleClass('d-lg-none');
-      $("#res-nav").toggleClass('d-lg-block');
-    },
-    function(){
-      $("#R").toggleClass('d-lg-none');
-      $("#res-nav").toggleClass('d-lg-block');
-    }
-  );
-  $("#project-nav").hover(
-    function(){
-      $("#P").toggleClass('d-lg-none');
-      $("#proj-nav").toggleClass('d-lg-block');
-    },
-    function(){
-      $("#P").toggleClass('d-lg-none');
-      $("#proj-nav").toggleClass('d-lg-block');
-    }
-  );
-  $("#connect-nav").hover(
-    function(){
-      $("#C").toggleClass('d-lg-none');
-      $("#conn-nav").toggleClass('d-lg-block');
-    },
-    function(){
-      $("#C").toggleClass('d-lg-none');
-      $("#conn-nav").toggleClass('d-lg-block');
-    }
-  );
-  
-// toggle img layout
-  $('#me').toggleClass("float-right")
-
-}
-
-
-if (window.matchMedia("(max-width:992px)").matches) {
-  $("#myname").toggleClass('offset-3');
-  $("#mydevtype").toggleClass('offset-3');
-  $(".pills").toggleClass('offset-2 mt-5');
-
-  // $(".email").toggleClass('offset-3');
-  // $(".email").toggleClass('ml-5');
-}
+// if (window.matchMedia("(max-width:992px)").matches) {
+//   $("#myname").toggleClass('offset-3');
+//   $("#mydevtype").toggleClass('offset-3');
+//   $(".pills").toggleClass('offset-2 mt-5');
+// }
