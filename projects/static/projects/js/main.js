5eed74787ca314ad96bcd45241ee383b6b5537e6
SolJCam <scameron110@gmail.com>
2019-08-18 16:45:18 -0400

    continuing to work on parallx behavior

--- projects/static/projects/js/main.js
+++ projects/static/projects/js/main.js
@@ -3,17 +3,95 @@ console.log("scripts are being read");
 //fade in my image and blurb
 $(document).ready(function (e) {
 
-  // create id's for project nav link and parallax scroll behavior
-  for(let i=0;i<=$(".parallax").length;i++){
-    $(".parallax").eq(i).attr("id", `#projects${i}`); // learn about the eq() method here: https://api.jquery.com/eq/
-    $(".proj-nav").eq(i).attr("id", `#proj-nav${i}`);
-  }
-
   $("#me").fadeIn(2e3);
   $("#blurb").fadeIn(2e3);
     
   console.log('2ndplz!');
-  
+
+
+
+  //Email submission functionality
+  $('#submit').click(function(e){
+    if($('#inputNameF').val()==""){
+      e.preventDefault();
+      alert("Please provide a name");
+      $("#inputNameF").css({"border-color":"red", "border-width": "3px"});
+    }else if($('#inputEmail').val()==""){
+      e.preventDefault();
+      $("#inputEmail").css({"border-color":"red", "border-width": "3px"});
+      alert("Please provide an email");
+    }else if($('#inputSubject').val()==""){
+      e.preventDefault();
+      $("#inputSubject").css({"border-color":"red", "border-width": "3px"});
+      alert("Please provide a subject");
+    }else if($('#inputMessage').val()==""){
+      e.preventDefault();
+      $("#inputMessage").css({"border-color":"red", "border-width": "3px"});
+      alert("Please provide a message");
+    }else{
+      var email = "mailto:scameron10@yahoo.com?from="+$('#inputEmail').val()+"&subject="+$('#inputSubject').val()+"&body=<"+$('#inputNameF').val()+" "+$('#inputNameS').val()+"><"+$('#inputEmail').val()+">"+$('#inputMessage').val();
+      var href = email.replace(/ /g, "%20");
+      console.log(href);
+      $("#email").attr("action", href);
+    }
+  });
+
+
+
+  //Social Media Link Behaviour
+  $("#first").hover(
+    function(){
+      $("#second").fadeTo("fast", 0.3);
+      $("#third").fadeTo("fast", 0.3);
+    },
+    function(){
+      $("#second").fadeTo("fast", 1);
+      $("#third").fadeTo("fast", 1);
+    }
+  );
+
+  $("#second").hover(
+    function(){
+      $("#first").fadeTo("fast", 0.3);
+      $("#third").fadeTo("fast", 0.3);
+    },
+    function(){
+      $("#first").fadeTo("fast", 1);
+      $("#third").fadeTo("fast", 1);
+    }
+  );
+
+  $("#third").hover(
+    function(){
+      $("#first").fadeTo("fast", 0.3);
+      $("#second").fadeTo("fast", 0.3);
+    },
+    function(){
+      $("#first").fadeTo("fast", 1);
+      $("#second").fadeTo("fast", 1);
+    }
+  );
+
+  // Not sure if this code is necessary. Browser resize suggests yes, Chrome dev tools suggests no. Will know after deployment
+
+  // if (window.matchMedia("(max-width:992px)").matches) {
+  //   $("#myname").toggleClass('offset-3');
+  //   $("#mydevtype").toggleClass('offset-3');
+  //   $(".pills").toggleClass('offset-2 mt-5');
+  // }
+
+
+
+  $("#projects-page").fadeIn(5e3);
+
+  const par_height = `${$(".parallax").height()}`
+  $(".parallax").css("min-height", (index)=>{return index = par_height});
+  $(".parallax").css("min-height", function(index){return index = par_height});
+
+  $(".parallax").css("min-height", par_height)
+  const par_mb = `${$(".parallax").css('min-height')}`
+  $(".parallax").css("margin-bottom", par_mb)
+
 });
 
 
@@ -72,26 +150,13 @@ if (window.matchMedia("(min-width:992px)").matches) {
   $(".email").toggleClass('mr-3 ml-3');
 
  
-  // nav project link functionality to scroll to individual projects on large viewports
-  $('#C').click(function(e){
-    e.preventDefault();
-    $("html,body").animate({
-      scrollTop: 1655
-    }, 215)
-  });
-  
-  
-  // PROJECT PAGE parallax behavior here
-  $(window).on('scroll', function(){
-
-    if (window.matchMedia('(min-width:900px)').matches){  
-
-      // console.log("it's hittin")
-      const scrolled = $(window).scrollTop();
-      $('.parallax').css('top', 0 - scrolled);
-
-    }
-  })
+  // nav project link BASIC functionality to scroll to individual projects on large viewports: work in progress
+  // $('#C').click(function(e){
+  //   e.preventDefault();
+  //   $("html,body").animate({
+  //     scrollTop: 1655
+  //   }, 215)
+  // });
 
 }
 
@@ -100,7 +165,7 @@ if (window.matchMedia("(min-width:992px)").matches) {
 
 if (window.matchMedia("(max-width:992px)").matches) {
   
-  // nav connect link functionality to scroll to social media links on small viewports
+  // nav connect link functionality to scroll to social media links on small viewports: wip
   $('#C').click(function(e){
     e.preventDefault();
     $("html,body").animate({
@@ -108,7 +173,7 @@ if (window.matchMedia("(max-width:992px)").matches) {
     }, 215)
   });
 
-  // nav project link functionality to scroll to individual projects on small viewports
+  // nav project link BASIC functionality to scroll to individual projects on small viewports
   // $('#C').click(function(e){
   //   e.preventDefault();
   //   $("html,body").animate({
@@ -116,84 +181,3 @@ if (window.matchMedia("(max-width:992px)").matches) {
   //   }, 215)
   // });
 }
-
-
-
-
-
-
-
-
-//Email submission functionality
-$('#submit').click(function(e){
-  if($('#inputNameF').val()==""){
-    e.preventDefault();
-    alert("Please provide a name");
-    $("#inputNameF").css({"border-color":"red", "border-width": "3px"});
-  }else if($('#inputEmail').val()==""){
-    e.preventDefault();
-    $("#inputEmail").css({"border-color":"red", "border-width": "3px"});
-    alert("Please provide an email");
-  }else if($('#inputSubject').val()==""){
-    e.preventDefault();
-    $("#inputSubject").css({"border-color":"red", "border-width": "3px"});
-    alert("Please provide a subject");
-  }else if($('#inputMessage').val()==""){
-    e.preventDefault();
-    $("#inputMessage").css({"border-color":"red", "border-width": "3px"});
-    alert("Please provide a message");
-  }else{
-    var email = "mailto:scameron10@yahoo.com?from="+$('#inputEmail').val()+"&subject="+$('#inputSubject').val()+"&body=<"+$('#inputNameF').val()+" "+$('#inputNameS').val()+"><"+$('#inputEmail').val()+">"+$('#inputMessage').val();
-    var href = email.replace(/ /g, "%20");
-    console.log(href);
-    $("#email").attr("action", href);
-  }
-});
-
-
-
-
-
-
-
-//Social Media Link Behaviour
-$("#first").hover(
-  function(){
-    $("#second").fadeTo("fast", 0.3);
-    $("#third").fadeTo("fast", 0.3);
-  },
-  function(){
-    $("#second").fadeTo("fast", 1);
-    $("#third").fadeTo("fast", 1);
-  }
-);
-
-$("#second").hover(
-  function(){
-    $("#first").fadeTo("fast", 0.3);
-    $("#third").fadeTo("fast", 0.3);
-  },
-  function(){
-    $("#first").fadeTo("fast", 1);
-    $("#third").fadeTo("fast", 1);
-  }
-);
-
-$("#third").hover(
-  function(){
-    $("#first").fadeTo("fast", 0.3);
-    $("#second").fadeTo("fast", 0.3);
-  },
-  function(){
-    $("#first").fadeTo("fast", 1);
-    $("#second").fadeTo("fast", 1);
-  }
-);
-
-// Not sure if this code is necessary. Browser resize suggests yes, Chrome dev tools suggests no. Will know after deployment
-
-// if (window.matchMedia("(max-width:992px)").matches) {
-//   $("#myname").toggleClass('offset-3');
-//   $("#mydevtype").toggleClass('offset-3');
-//   $(".pills").toggleClass('offset-2 mt-5');
-// }
