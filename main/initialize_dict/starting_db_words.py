20c720dc9c74a6f2d99c22e56a06667bc88262ac
SolJCam <scameron110@gmail.com>
2019-09-18 23:57:34 -0400

    continuing to work on parsing json dictionary 'word' file

--- main/initialize_dict/starting_db_words.py
+++ main/initialize_dict/starting_db_words.py
@@ -3,18 +3,20 @@ import json
 
 
 def display_words():
-    with open("./dictionary.json") as f:
+    # with open("./dictionary.json") as f:
 
-      for word in f:
-        display = "<p>{0}</p><br>".format(word)
+    #   for word in f:
+    #     display = "<p>{0}</p><br>".format(word)
       
-      nu_file = open("./test.html", "w")
-      nu_file.write(display)
+    #   nu_file = open("./test.html", "w")
+    #   nu_file.write(display)
     
     json_words = open("./dictionary.json")
     read_json = json_words.read()
     dict_words = json.loads(read_json)
     json_words.close()
-    return print(dict_words)
+
+    for word in dict_words:
+      return print(word, dict_words.word)
 
 display_words()
