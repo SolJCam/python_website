79242f9b764a14ed381b81a0d6db347c3ca0ed67
SolJCam <scameron110@gmail.com>
2019-09-22 23:49:25 -0400

    wrote test to parse json file. Minor updates to html and static/img directory

--- main/initialize_dict/starting_db_words.py
+++ main/initialize_dict/starting_db_words.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+# from django.shortcuts import get_object_or_404, render
 from main.models import Word
 import json
 
@@ -6,11 +6,11 @@ import json
 def display_words():
     # with open("./dictionary.json") as f:
 
-    #   for word in f:
-    #     display = "<p>{0}</p><br>".format(word)
+      # for word in f:
+      #   display = "<p>{0}</p><br>".format(word)
       
-    #   nu_file = open("./test.html", "w")
-    #   nu_file.write(display)
+      # nu_file = open("./test.html", "w")
+      # nu_file.write(display)
     
     json_words = open("./dictionary.json")
     read_json = json_words.read()
@@ -18,17 +18,23 @@ def display_words():
     json_words.close()
 
     for word in dict_words:
-      if dict_words.word[1]:
-        second = dict_words.word[1]
-      elif dict_words.word[2]:
-        third = dict_words.word[2]
-      elif len(dict_words.word) >3:
-        diff = len(dict_words.word) - 3
-        additional = [ dict_words.word[2+x] for x in range(diff) ]
-      nu_word = Word.objects.create(name=word, first_definition=dict_words.word[0], second_definition=second, third_definition=third, more_definitions=additional)
-    
-    nu_word.save()
+      if word != Word.objects.get(name=word):
+        if dict_words[word[1]]:
+          second = dict_words[word[1]]
+        elif dict_words[word[2]]:
+          third = dict_words[word[2]]
+        elif len(dict_words[word])>3:
+          diff = len(dict_words[word])-3
+          additional = [ dict_words[word[2+x]] for x in range(diff) ]
+        
+        nu_word = Word.objects.create(name=word, first_definition=dict_words.word[0], second_definition=second, third_definition=third, more_definitions=additional)
+        nu_word.save()
 
     return print(nu_word)
 
 display_words()
+
+
+
+
+small_d = dict(list(dict_words.items())[:11])
--- main/test.py
+++ main/test.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from main.models import Project, Word
 from django.urls import reverse
+import json
 
 # to test only one method: ./manage.py test main.tests.ProjectTestCase.<method>  
 
@@ -29,4 +30,25 @@ class ProjectTestCase(TestCase):
     # Test ability to create Word objects    
     def create_word_test(self):
         word = Word.objects.create(name="ether", first_definition="A colorless liquid, slightly soluble in water; used as a reagent, intermediate, anesthetic, and solvent.\\n(Source: MGH)", second_definition="A class of chemical compounds which contain an oxygen atom connected to two (substituted) alkyl groups.", third_definition="", more_definitions="(substituted) alkyl groups.")
+
+    def make_dict(self):
+        json_words = open("./initialize/dictionary.json")
+        read_json = json_words.read()
+        dict_words = json.loads(read_json)
+        json_words.close()
+
+        for word in dict_words:
+            if word != Word.objects.get(name=word):
+                if dict_words.word[1]:
+                    second = dict_words.word[1]
+                elif dict_words.word[2]:
+                    third = dict_words.word[2]
+                elif len(dict_words.word) >3:
+                    diff = len(dict_words.word) - 3
+                    additional = [ dict_words.word[2+x] for x in range(diff) ]
+                
+                nu_word = Word.objects.create(name=word, first_definition=dict_words.word[0], second_definition=second, third_definition=third, more_definitions=additional)
+                nu_word.save()
+
+            return print(nu_word)
 
