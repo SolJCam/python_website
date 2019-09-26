1f6ce6e5ca20db3780233be608de44a8f29075f1
SolJCam <scameron110@gmail.com>
2019-09-26 19:20:12 -0400

    testing for code logic to parse json file complete. Works perfectly

--- main/initialize_dict/starting_db_words.py
+++ main/initialize_dict/starting_db_words.py
@@ -1,5 +1,6 @@
 # from django.shortcuts import get_object_or_404, render
-from main.models import Word
+# from main.models import Word
+from "..models" import Word
 import json
 import pdb 
 
@@ -16,21 +17,28 @@ def display_words():
   read_json = json_words.read()
   dict_words = json.loads(read_json)
   json_words.close()
+  print(len())
   # small_d = dict(list(dict_words.items())[:11])
 
   for word in dict_words:
-    # pdb.set_trace()
+      # pdb.set_trace()
 
     first = dict_words[word][0]
     if len(dict_words[word])>1 and len(dict_words[word])<2:
-        second = dict_words[word[1]]
+        second = dict_words[word][1]
         nu_word = Word.objects.using('dictionary').create(name=word, first_definition=first, second_definition=second, third_definition="", more_definitions="")
     elif len(dict_words[word])>2 and len(dict_words[word])<3:
-        third = dict_words[word[2]]
+        second = dict_words[word][1]
+        third = dict_words[word][2]
         nu_word = Word.objects.using('dictionary').create(name=word, first_definition=first, second_definition=second, third_definition=third, more_definitions="")
     elif len(dict_words[word])>3:
+        # pdb.set_trace()
+        second = dict_words[word][1]
+        third = dict_words[word][2]
+        additional = []
         diff = len(dict_words[word])-3
-        additional = [ dict_words[word[2+x]] for x in range(diff) ]
+        for x in range(diff):
+            additional.append(dict_words[word][3+x])
         nu_word = Word.objects.using('dictionary').create(name=word, first_definition=first, second_definition=second, third_definition=third, more_definitions=additional)
     else:
         nu_word = Word.objects.using('dictionary').create(name=word, first_definition=first, second_definition="", third_definition="", more_definitions="")
