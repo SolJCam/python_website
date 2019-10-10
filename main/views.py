0876d2819497cad174eb92a9064c4fe697b0655f
SolJCam <scameron110@gmail.com>
2019-10-09 23:35:26 -0400

    I dunno...

--- main/views.py
+++ main/views.py
@@ -16,71 +16,57 @@ def site_index(request):
   # pdb.set_trace()
   return render(request, 'index.html', context)
 
+# def project_index(request):
+#   projects = Project.objects.all()
+
+#   if request.method == 'POST':
+#     # create a form instance and populate it with data from the request:
+#     req = DictForm(request.POST)
+    
+#     # check whether it's valid:
+#     if req.is_valid():
+#       # If True, will be able to find all the validated form data in its cleaned_data attribute and use it to update the database etc
+#       form = Word.objects.using('dictionary').get(name=req.cleaned_data['py_dictionary'])
+
+
+#   # if a GET (or any other method) we'll create a blank form
+#   else:
+#     form = DictForm() 
+
+#   context = {
+#       'projects': projects,
+#       'form': form,
+#   }
+
+#   return render(request, 'local_apps.html', context)
+
+
 def project_index(request):
   projects = Project.objects.all()
 
-  if request.method == 'POST':
-    # create a form instance and populate it with data from the request:
+  if request.method == 'GET':
     req = DictForm(request.GET)
-    
-    # check whether it's valid:
-    if req.is_valid():
-      # If True, will be able to find all the validated form data in its cleaned_data attribute and use it to update the database etc
-      form = Word.objects.using('dictionary').get(name=req.cleaned_data['py_dictionary'])
 
-      context = {
-        'projects': projects,
-        'form': form,
-      }
+    # determine if template form request has input data attached
+    if req.is_bound:
 
-      return render(request, 'local_apps.html', context)
+      # if no data attached, create an empty form instance and to send to browser:
+      form = DictForm()
 
-
-  # if a GET (or any other method) we'll create a blank form
-  else:
-    form = DictForm() 
-
+    else: # if data is attached then...   
+      # check whether it's valid:
+      if req.is_valid():
+        
+        # If True, will be able to find all the validated form data in its cleaned_data attribute and use it to update the database etc
+        form = Word.objects.using('dictionary').get(name=req.cleaned_data['py_dictionary'])
+  # else:
+     
   context = {
       'projects': projects,
       'form': form,
   }
 
   return render(request, 'local_apps.html', context)
-
-
-# model = Word
-# fields = [
-#   "name",
-#   "first_definition",
-#   "first_ex",
-#   "second_definition",
-#   "second_ex",
-#   "third_definition",
-#   "third_ex",
-#   "synonym",
-# ]
-
-# def WordCreate(CreateView):
-#   model
-#   fields 
-
-# def WordUpdate(UpdateView):
-#   model
-#   fields
-
-# def WordDelete(DeleteView):
-#   model
-
-
-
-
-
-
-
-
-
-
-
 
 
 # Below will be to visit indivudual project urls. Not ready
