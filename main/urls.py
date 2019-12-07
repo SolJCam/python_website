514980238bed96c891f525cc641e5f64dd6582e5
SolJCam <scameron110@gmail.com>
2019-12-06 23:57:16 -0500

    minor edits

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,5 @@ from . import  views
 urlpatterns = [
     path("", views.site_index, name="site_index"),
     path("projects/", views.project_index, name="project_index"),
-    path("projects/<int:pk>", views.project_detail, name="project_detail") #<int:pk> notation tells Django that the value passed in the URL is an integer, and its variable name is pk.
+    # path("projects/<int:pk>", views.project_detail, name="project_detail") #<int:pk> notation tells Django that the value passed in the URL is an integer, and its variable name is pk.
 ]
