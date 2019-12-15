2d643ae44458b964d622e46fd4b074b541935046
SolJCam <scameron110@gmail.com>
2019-12-14 23:50:58 -0500

    minor edits

--- py_scraper/static/py_scraper/js/py_scraper.js
+++ py_scraper/static/py_scraper/js/py_scraper.js
@@ -15,7 +15,7 @@ console.log("scripts are being read");
 // }
 
 async function generateCloud (){
-    fetch("https://textvis-word-cloud-v1.p.rapidapi.com/v1/textToCloud?max_words=200&font=Tahoma&language=en&colors=%255B'%2523375E97'%252C%20'%2523FB6542'%252C%20'%2523FFBB00'%252C%20'%25233F681C'%255D&use_stopwords=true&scaling=0.5&width=800&height=800&text=This%20is%20a%20test.%20I%20repeat%252C%20this%20is%20a%20test.%20We%20are%20only%20testing%20the%20functionality%20of%20this%20api%252C%20nothing%20else.%20End%20of%20test.", {
+    await fetch("https://textvis-word-cloud-v1.p.rapidapi.com/v1/textToCloud?max_words=200&font=Tahoma&language=en&colors=%255B'%2523375E97'%252C%20'%2523FB6542'%252C%20'%2523FFBB00'%252C%20'%25233F681C'%255D&use_stopwords=true&scaling=0.5&width=800&height=800&text=This%20is%20a%20test.%20I%20repeat%252C%20this%20is%20a%20test.%20We%20are%20only%20testing%20the%20functionality%20of%20this%20api%252C%20nothing%20else.%20End%20of%20test.", {
 	"method": "POST",
 	"headers": {
 		"x-rapidapi-host": "textvis-word-cloud-v1.p.rapidapi.com",
@@ -47,14 +47,9 @@ async function generateCloud (){
     // .then(text => {
     //     return text.text();
 	// })
-    // .then(parsed => {
-    //     console.log(parsed);
-	// })
-	.then(res => {
-		res;
-		console.log(res);
-	})          
-	.then(text => {return text})    
+    .then(parsed => {
+        console.log(parsed);
+	})   
 	.catch(err => {
         console.log(err);
     });
