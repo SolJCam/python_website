4e57bae0bfc03e8a1874afe5256a3585ab04b441
SolJCam <scameron110@gmail.com>
2019-12-16 23:50:59 -0500

    changed api

--- py_scraper/static/py_scraper/js/py_scraper.js
+++ py_scraper/static/py_scraper/js/py_scraper.js
@@ -14,51 +14,44 @@ console.log("scripts are being read");
 //     };
 // }
 
-async function generateCloud (){
-    await fetch("https://textvis-word-cloud-v1.p.rapidapi.com/v1/textToCloud?max_words=200&font=Tahoma&language=en&colors=%255B'%2523375E97'%252C%20'%2523FB6542'%252C%20'%2523FFBB00'%252C%20'%25233F681C'%255D&use_stopwords=true&scaling=0.5&width=800&height=800&text=This%20is%20a%20test.%20I%20repeat%252C%20this%20is%20a%20test.%20We%20are%20only%20testing%20the%20functionality%20of%20this%20api%252C%20nothing%20else.%20End%20of%20test.", {
-	"method": "POST",
-	"headers": {
-		"x-rapidapi-host": "textvis-word-cloud-v1.p.rapidapi.com",
-		"x-rapidapi-key": "b61f4c9fc0msh384299504cd20a5p19e082jsnede2583ed2ed",
-		"x-api-key": "qLn10OtYwY8MILjfYAIH11rx6yVWaGMOUNh0NZQh",
-		"content-type": "application/json",
-		"accept": "application/json"
-	},
-	"body": {
-		"text": "This is a test. I repeat, this is a test. We are only testing the functionality of this api, nothing else. End of test.",
-		"scale": 0.5,
-		"width": 800,
-		"height": 800,
-		"colors": [
-			"#375E97",
-			"#FB6542",
-			"#FFBB00",
-			"#3F681C"
-		],
-		"font": "Tahoma",
-		"use_stopwords": true,
-		"language": "en",
-		"uppercase": false
-	}
-    })
-    .then(response => {
-        return response.text();
-    })
-    // .then(text => {
-    //     return text.text();
-	// })
-    .then(parsed => {
-        console.log(parsed);
-	})   
+function generateCloud (){
+    fetch("https://wordcloudservice.p.rapidapi.com/generate_wc", {
+		"method": "POST",
+		"headers": {
+			"x-rapidapi-host": "wordcloudservice.p.rapidapi.com",
+			"x-rapidapi-key": "b61f4c9fc0msh384299504cd20a5p19e082jsnede2583ed2ed",
+			"content-type": "application/json",
+			"accept": "application/json"
+		},
+		"body": {
+			"f_type": "png",
+			"width": 800,
+			"height": 500,
+			"s_max": "7",
+			"s_min": "1",
+			"f_min": 1,
+			"r_color": "TRUE",
+			"r_order": "TRUE",
+			"s_fit": "FALSE",
+			"fixed_asp": "TRUE",
+			"rotate": "TRUE",
+			"textblock": "generate word cloud generate word cloud awesome great png jpg pdf awesome generate word cloud"
+		}
+	})
+	.then(response => {
+		console.log(response);
+	})
 	.catch(err => {
-        console.log(err);
-    });
+		console.log(err);
+	});
 }
 
 $(document).ready(e => {
     $("#scrape").click(e=>{
         e.preventDefault();
-        let cloud = generateCloud();
-        // debugger
+		let cloud = generateCloud();
+		console.log(cloud);
+		// debugger
+		// $("#wordcloud").attr("src", cloud);
     })
 })
