0c155ceaf0ed4227d6d6720f8317b9776a7d0054
SolJCam <scameron110@gmail.com>
2019-12-21 21:01:57 -0500

    wordcloud api already does everything I planned to build

--- py_scraper/static/py_scraper/js/py_scraper.js
+++ py_scraper/static/py_scraper/js/py_scraper.js
@@ -1,60 +1,30 @@
 console.log("scripts are being read");
 
-// async function scrapeNews(){
-//     let cnnResponse = await fetch(){
-
-//     };
-
-//     let msnbcResponse = await fetch(){
-
-//     };
-
-//     let foxResponse = await fetch(){
-
-//     };
-// }
-
-async function generateCloud (){
-	
-	await fetch("https://wordcloudservice.p.rapidapi.com/generate_wc", {
-		"method": "POST",
-		"headers": {
-			"x-rapidapi-host": "wordcloudservice.p.rapidapi.com",
-			"x-rapidapi-key": "b61f4c9fc0msh384299504cd20a5p19e082jsnede2583ed2ed",
-			"content-type": "application/json",
-			"accept": "application/json"
-		},
-		"body": {
-			"f_type": "png",
-			"width": 800,
-			"height": 500,
-			"s_max": "4",
-			"s_min": ".5",
-			"f_min": 3,
-			"r_color": "TRUE",
-			"r_order": "FALSE",
-			"s_fit": "FALSE",
-			"fixed_asp": "TRUE",
-			"rotate": "TRUE",
-			"url": "https://en.wikipedia.org/wiki/Canada",
-			"remove_words": "canada canadian original archived"
-		}
-	})
-	.then(response => {
-		console.log(response);
-	})
-	.catch(err => {
-		console.log(err);
-	});
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://wordcloudservice.p.rapidapi.com/generate_wc",
+	"method": "POST",
+	"headers": {
+		"x-rapidapi-host": "wordcloudservice.p.rapidapi.com",
+		"x-rapidapi-key": "77632acd7emsh25a7f96161cc49cp19dd85jsnd720e55b6ed1",
+		"content-type": "application/json",
+		"accept": "application/json"
+	},
+	"processData": false,
+	"data": "{  \"f_type\": \"png\",  \"width\": 800,  \"height\": 500,  \"s_max\": \"4\",  \"s_min\": \".5\",  \"f_min\": 3,  \"r_color\": \"TRUE\",  \"r_order\": \"FALSE\",  \"s_fit\": \"FALSE\",  \"fixed_asp\": \"TRUE\",  \"rotate\": \"TRUE\",  \"url\":\"https://www.npr.org/\",  \"remove_words\":\"\"}"
 }
+
 
 $(document).ready(e => {
     $("#scrape").click(e=>{
         e.preventDefault();
-		let cloud = generateCloud();
-		console.log(cloud);
-		debugger
-		// $("#wordcloud").attr("src", cloud);
+
+		$.ajax(settings).done(function (response) {
+			// debugger
+			console.log(response);
+			$("#wordcloud").attr("src", response['url']);
+		});
     })
 })
 
