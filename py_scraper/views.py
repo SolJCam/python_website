83ca4b303efc6fc0d0f4cec6cafcaddfe50ee235
SolJCam <scameron110@gmail.com>
2020-01-16 22:49:29 -0500

    updated projects page with py_scraper image and scroll_to js

--- py_scraper/views.py
+++ py_scraper/views.py
@@ -61,6 +61,7 @@ def scrape_msnbc(request):
   for ec in ele:
       for text in ec:
           msnbcfile.write(text.text)
+      pdb.set_trace()
   msnbcfile.close()
   # run word cloud generator and return result or raise internal server error exception
   try:
@@ -86,6 +87,7 @@ def scrape_cnn(request):
                   continue
               else:
                   cnnfile.write(cnnele.text)
+              pdb.set_trace()
           except:
               continue
   cnnfile.close()
@@ -106,7 +108,8 @@ def scrape_fox(request):
           continue
       else:
           foxfile.write(tag.text)
-
+      pdb.set_trace()
+      
   foxfile.close()
   try:
     wcgenerator("foxnews.txt", "fox.jpeg", "foxwrdcld.png")
