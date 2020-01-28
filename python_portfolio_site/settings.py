c2179cbaa56d954287ae716bdc1edc197b850a87
SolJCam <scameron110@gmail.com>
2020-01-27 22:09:35 -0500

    moved staticfiles BACK to top level directory

--- python_portfolio_site/settings.py
+++ python_portfolio_site/settings.py
@@ -126,7 +126,7 @@ PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
 SITE_ROOT = os.path.dirname(PROJECT_ROOT)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
