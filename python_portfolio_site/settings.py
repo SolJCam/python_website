24d864b748c9b9d50c6aa151356f72a39477c156
SolJCam <scameron110@gmail.com>
2020-01-28 19:22:06 -0500

    corrected BASE_DIR path and repaired collectstatic error

--- python_portfolio_site/settings.py
+++ python_portfolio_site/settings.py
@@ -126,17 +126,17 @@ PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
 SITE_ROOT = os.path.dirname(PROJECT_ROOT)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'staticfiles'),
+# )
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
---
+++ python_portfolio_site/staticfiles/py_scraper/js/py_scraper.js
@@ -0,0 +1,65 @@
+console.log("scripts are being read");
+
+
+$(document).ready(e => {
+    $("#scrape_msnbc").click(e=>{
+		e.preventDefault();
+		
+		fetch("scrape_msnbc")
+			.then((response)=>{
+				return response.json();
+			})
+			.then((json)=>{
+				$('#msnbcwrdcld').attr('src', '/static/imgs/msnbcwrdcld.png');
+				$('#msnbcwrdcld').load(" #msnbcwrdcld");
+				for(let i=0;i<=4;i++){
+					$(`#ms-${i}`).text(`${json[i][0]} : ${json[i][1]}`);
+				}
+			})
+			.catch((error)=>{
+				alert(error+" : Please try again");
+			})
+	})
+
+
+    $("#scrape_cnn").click(e=>{
+		e.preventDefault();
+		
+		fetch("scrape_cnn")
+			.then((response)=>{
+				return response.json();
+			})
+			.then((json)=>{
+				$('#cnnwrdcld').attr('src', '/static/imgs/cnnwrdcld.png');
+				$('#cnnwrdcld').load(" #cnnwrdcld");
+				for(let i=0;i<=4;i++){
+					$(`#cnn-${i}`).text(`${json[i][0]} : ${json[i][1]}`);
+				}
+			})
+			.catch((error)=>{
+				alert(error+" : Please try again");
+			})
+	})
+	
+
+    $("#scrape_fox").click(e=>{
+		e.preventDefault();
+		
+		fetch("scrape_fox")
+			.then((response)=>{
+				return response.json();
+			})
+			.then((json)=>{
+				$('#foxwrdcld').attr('src', '/static/imgs/foxwrdcld.png');
+				$('#foxwrdcld').load(" #foxwrdcld");
+				for(let i=0;i<=4;i++){
+					$(`#fox-${i}`).text(`${json[i][0]} : ${json[i][1]}`);
+				}
+			})
+			.catch((error)=>{
+				alert(error+" : Please try again");
+			})
+    })
+})
+
+
