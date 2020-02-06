18324b037af4214982623aee26fe3c38e56558a2
SolJCam <scameron110@gmail.com>
2020-02-05 22:54:29 -0500

    included options.add_argument(--disable-dev-shm-usage) to address 'DevToolsActivePort file doesn't exist' error

--- py_scraper/views.py
+++ py_scraper/views.py
@@ -11,6 +11,7 @@ d = os.path.dirname(__file__) if "__file__" in locals() else os.getcwd()
 
 # Selenium configuration for Heroku
 options = webdriver.ChromeOptions()
+options.add_argument("--disable-dev-shm-usage")
 options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
 driver = webdriver.Chrome(chrome_options=options,executable_path=os.environ.get('CHROMEDRIVER_PATH'))
 
