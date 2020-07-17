9d91f65cdd3cdf39e6454e68f74e2c43fd85335b
SolJCam <scameron110@gmail.com>
2020-07-16 23:30:45 -0400

    printing env var

--- main/views.py
+++ main/views.py
@@ -41,7 +41,7 @@ def site_index(request):
     # yag = yagmail.SMTP(server_email, oauth2_file=oauth)
     
     psswrd = os.environ["GMAIL_PSSWRD"]
-    print(type(psswrd))
+    print(psswrd)
     yag = yagmail.SMTP(server_email, psswrd) 
     # yag = yagmail.SMTP(server_email, oauth2_file="main/oauth2_creds.json")  # oauth parameter actually generates this file; no need to create
     # pdb.set_trace()
