1d17726cded9490f839523af22284ab175060e4e
SolJCam <scameron110@gmail.com>
2020-07-23 10:26:21 -0400

    running a test removing everything not necessary to send yagmail

--- main/views.py
+++ main/views.py
@@ -46,10 +46,13 @@ def site_index(request):
     # yag = yagmail.SMTP(server_email, oauth2_file="main/oauth2_creds.json")  # oauth parameter actually generates this file; no need to create
     # pdb.set_trace()
     yag.send(
-      to=server_email,
-      subject=Subject,
-      contents=body, 
+      to=server_email
     )
+    # yag.send(
+    #   to=server_email,
+    #   subject=Subject,
+    #   contents=body, 
+    # )
     print(new_email)
 
   context = {
