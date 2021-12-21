18f798481c1c94b9af03a32a5816ba45aa4f26f9
SolJCam <scameron110@gmail.com>
2021-12-20 20:21:36 -0500

    clock appears to be instantiated but the code doesn't appear to be executed. Trying something different

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ import yagmail, json, os, csv, pdb #python debugger
 from .models import Project, Word
 from .forms import InputForm, DictForm
 from .view_functions import get_meaning, add_word
-# from .git_api_scheduler import schedule_api_call
+from .git_api_scheduler import schedule_api_call
 # from .git_api import git_api
 
 d = os.path.dirname(__file__) if "__file__" in locals() else os.getcwd()
@@ -49,7 +49,7 @@ def git_notifications(request):
   # if err_resp != "success":
   #   return JsonResponse(err_resp, safe=False)
   
-  # schedule_api_call()
+  schedule_api_call()
   
   response = {}
   try:
